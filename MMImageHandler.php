1bb53752500d7d9b4ae8ccc82f02bc731961e5a6
Vitaliy Filippov <vitalif@yourcmc.ru>
2009-11-18 13:50:11 +0000

    Bug 29523 90% + customizable width and height + link

--- MMImageHandler.php
+++ MMImageHandler.php
@@ -21,12 +21,12 @@ class MMPlayCode extends MediaTransformOutput
     /**
      * Constructor
      */
-    function MMPlayCode ($file, $url, $width, $height, $path = false, $page = false)
+    function MMPlayCode ($file, $url, $params, $path = false, $page = false)
     {
         $this->file = $file;
         $this->url = $url;
-        $this->width = 0+$width;
-        $this->height = 0+$height;
+        $this->width = $params['width'];
+        $this->height = $params['height'];
         $this->path = $path;
         $this->page = $page;
     }
@@ -67,18 +67,24 @@ class MMPlayCode extends MediaTransformOutput
         $w = $this->width;
         $h = $this->height;
 
+        global $wgUser;
+        $title = $this->file->getTitle();
+        $link = $wgUser->getSkin()->link($title);
+
+        # TODO png thumbnails if $params['noflash']
+
         return <<<EOF
-$prefix<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="90%" height="$h">
+$prefix<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="$w" height="$h">
     <param name="movie" value="$wgVisorFreemind" />
     <param name="allowfullscreen" value="true" />
     <param name="flashvars" value="openUrl=_blank&initLoadFile=$strURL&startCollapsedToLevel=5" />
     <param name="quality" value="high" />
     <param name="bgcolor" value="#FFFFFF" />
-    <embed type="application/x-shockwave-flash" width="90%" height="$h"
+    <embed type="application/x-shockwave-flash" width="$w" height="$h"
         allowfullscreen="true"
         src="$wgVisorFreemind"
         flashvars="openUrl=_blank&initLoadFile=$strURL&startCollapsedToLevel=5" />
-</object>$postfix
+</object><p>$link</p>$postfix
 EOF;
     }
 }
@@ -95,7 +101,7 @@ class MMImageHandler extends ImageHandler
 
     function getImageSize($image, $filename)
     {
-        return array(640, 480);
+        return array(-1, -1);
     }
 
     function mustRender($file)
@@ -105,11 +111,21 @@ class MMImageHandler extends ImageHandler
 
     function normaliseParams($image, &$params)
     {
-        if (!parent::normaliseParams($image, $params))
-            return false;
-
-        $params['physicalWidth'] = $params['width'];
-        $params['physicalHeight'] = $params['height'];
+        if ($params['width'] == -1)
+            unset($params['width']);
+        if ($params['height'] == -1)
+            unset($params['height']);
+        $params['nolink'] = $params['nolink'] ? true : false;
+        if (!$params['width'])
+            $params['width'] = '90%';
+        else if ($params['height'] &&
+            substr($params['height'], -1) == '%' &&
+            substr($params['width'], -1) != '%')
+            $params['height'] = intval($params['width'] * intval(substr($params['width'], 0, -1)) / 100);
+        if (!$params['height'])
+            $params['height'] = 480;
+        if ($params['makeflvthumbnail'] || $params['imagegallery'])
+            $params['noflash'] = true;
         return true;
     }
 
@@ -117,14 +133,7 @@ class MMImageHandler extends ImageHandler
     {
         if (!$this->normaliseParams($image, $params))
             return new TransformParameterError($params);
-
-        $clientWidth = $params['width'];
-        $clientHeight = $params['height'];
-        $physicalWidth = $params['physicalWidth'];
-        $physicalHeight = $params['physicalHeight'];
-        $srcPath = $image->getPath();
-
-        return new MMPlayCode($image, $dstUrl, $clientWidth, $clientHeight, $dstPath);
+        return new MMPlayCode($image, $dstUrl, $params, $dstPath, false);
     }
 
     function getThumbType($ext, $mime)
